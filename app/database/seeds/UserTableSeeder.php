f8b1c41ffdf624223cacac7cda084ef166b61c07
Balazs <balazs129@gmail.com>
2015-01-29 17:28:19 +0100

    user table seeder

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+class UserTableSeeder extends Seeder {
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(array(
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'organization' => 'Test Organization',
+            'password' => Hash::make('rpw'),
+            'admin' => 0,
+        ));
+    }
+}
+
