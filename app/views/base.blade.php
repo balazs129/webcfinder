f3b2e1b7b13e503276666a8bee202309b7ec6de5
Balazs <balazs129@gmail.com>
2015-02-09 21:27:02 +0100

    css path fix

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
 
-        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/main.min.css" rel="stylesheet">
 
         @yield('head')
     </head>
--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="container">
-        <p class="text-muted text-center">Webcfinder alpha v0.4.3</p>
+        <p class="text-muted text-center">Webcfinder alpha v0.4.4</p>
     </div>
 </footer>
